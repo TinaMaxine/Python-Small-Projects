bffd49dfcc90af0fabd2e3038fdaa191d7d16e51
Anjani Priyanka <113289194+TinaMaxine@users.noreply.github.com>
2023-01-10 23:23:55 +0530

    Simple Parking Cost Calculator

---
+++ Parking cost calc.py
@@ -0,0 +1,10 @@
+
+#Getting Basic Input from user
+X=int(input("Enter no of two Wheeler came today "))
+Y=int(input("Enter no of four Wheeler came today "))
+M=int(input("Enter cost of two Weheler parking "))
+N=int(input("Enter cost of four Weheler parking "))
+#calculations 
+total=X*M+Y*N
+#final Printout
+print("total cost is",total)
