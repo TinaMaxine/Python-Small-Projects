c37b4f2499841dc5600d4602d29bbc8fdfcbc40a
Anjani Priyanka <113289194+TinaMaxine@users.noreply.github.com>
2023-01-10 23:23:06 +0530

    Program To check sum of odd numbers in Python

---
+++ sumofodd.py
@@ -0,0 +1,19 @@
+
+
+
+a=int(input("enter how long you want ot go "))
+b=int(input("enter differnce you want"))
+c=int(input("enter starting point"))
+sum=0
+for i in range(c,a,b):
+    print(i)
+    sum=sum+i
+    print(sum)
+print(sum)    
+    
+              
+
+    
+
+   
+
